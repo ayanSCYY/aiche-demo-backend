df837494c8327cd7a14dfd6f4d9c8bd50bfe4070
ayussh-2 <ayushatschool5@gmail.com>
2024-06-26 13:49:05 +0530

    chore: added events

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import db from "./db.js";
 import { typeDefs } from "./schema.js";
 
 let userDetails = db.users;
+let events = db.events;
+let registrations = db.eventRegistrations;
 
 // resolvers
 const resolvers = {
@@ -17,6 +19,46 @@ const resolvers = {
         },
         getUser(_, args) {
             return userDetails.find((user) => user.uid === args.uid);
+        },
+        getEvents(_, args) {
+            return events.filter((event) => event.orgID === args.orgID);
+        },
+        eventRegistration(_, args) {
+            const userEventRegistrations = registrations.filter(
+                (eventRegistration) => {
+                    return eventRegistration.userID === args.userID;
+                }
+            );
+
+            const filteredEventRegistrations = userEventRegistrations.filter(
+                (eventRegistration) => {
+                    return events.some((event) => {
+                        return (
+                            event.id === eventRegistration.eventID &&
+                            event.orgID === args.orgID
+                        );
+                    });
+                }
+            );
+            return filteredEventRegistrations;
+        },
+        events() {
+            const eventIDs = events.map((event) => {
+                if (event.orgID === "aiche") {
+                    return event.id;
+                }
+            });
+            return {
+                eventIDs: eventIDs || [],
+            };
+        },
+        teamRegistrationsByUser(_, args) {
+            return registrations.filter((registration) => {
+                return registration.userIDs?.includes(args.userID);
+            });
+        },
+        getTotalRegistered() {
+            return registrations;
         },
     },
 
@@ -30,6 +72,23 @@ const resolvers = {
 
             userDetails.push(newUser);
             return newUser;
+        },
+        createEventRegistration(_, args) {
+            const newEventRegistration = {
+                id: String(registrations.length + 1),
+                ...args.eventRegistration,
+            };
+
+            registrations.push(newEventRegistration);
+            return newEventRegistration;
+        },
+        createTeamEventRegistration(_, args) {
+            const newTeamEventRegistration = {
+                id: String(registrations.length + 1),
+                ...args.teamEventRegistration,
+            };
+            registrations.push(newTeamEventRegistration);
+            return newTeamEventRegistration;
         },
     },
 };
--- schema.js
+++ schema.js
@@ -15,13 +15,76 @@ export const typeDefs = `#graphql
         createdAt: String!
     }
 
+    type Event{
+        orgID: String!
+        id: ID!
+        name: String!
+        subHeading: String!
+        prizeMoney: String!
+        description: [String!]!
+        poster: String!
+        rules: String!
+        isTeamEvent: Boolean!
+        maxTeamSize: Int
+        minTeamSize: Int
+    }
+
+    type Events{
+        eventIDs: [String!]!
+    }
+
+    type EventRegistration {
+        id: ID!
+        eventID: ID!
+        userID: ID!
+        
+    }
+
+    type TeamEventRegistration {
+        id: ID!
+        eventID: ID!
+        teamName: String!
+        userIDs: [ID!]!
+    }
+
+    type EventRegs{
+        eventID: ID!
+        userID: ID
+        userIDs: [ID]
+        teamName: String
+    }
+
     type Query {
         getUser(uid: String): User
         users: [User!]!
+        getEvents(orgID: String!, pagination: PaginationInputType): [Event]
+        eventRegistration(userID: ID, orgID: ID): [EventRegistration]
+        events: Events
+        teamRegistrationsByUser(userID: ID!): [TeamEventRegistration]
+        getTotalRegistered: [EventRegs]
     }
 
     type Mutation {
         createUser(user: UserCreateInputType!): User
+        createEventRegistration(eventRegistration: EventRegistrationCreateInputType!): EventRegistration
+        createTeamEventRegistration(teamEventRegistration: TeamEventRegistrationCreateInputType!): TeamEventRegistration
+    }
+
+    input TeamEventRegistrationCreateInputType {
+        eventID: ID!
+        teamName: String!
+        userIDs: [ID!]!
+    }
+
+
+    input EventRegistrationCreateInputType {
+        eventID: ID!
+        userID: ID!
+    }
+
+    input PaginationInputType {
+        limit: Int
+        offset: Int
     }
 
     input UserCreateInputType {
